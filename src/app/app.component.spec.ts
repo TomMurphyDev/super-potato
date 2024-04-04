ab641177d86cac9853df2839fb3b2a41b4bedc98
Thomas Murphy <thomasmurphy1991@gmail.com>
2024-04-04 18:58:47 -0400

    tweaks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TomMurphyDev'`, () => {
+  it(`should have as title 'myApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TomMurphyDev');
+    expect(app.title).toEqual('myApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TomMurphyDev app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myApp app is running!');
   });
 });
